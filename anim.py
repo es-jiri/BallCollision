07567eda6d106c5da75c62f9f83b8732aa3fb530
jiri.strouhal <Jiri.Strouhal@vutbr.cz>
2022-01-12 17:28:40 +0100

    Four particless actually

--- anim.py
+++ anim.py
@@ -1,6 +1,9 @@
 from abc import abstractstaticmethod
 import turtle
 from math import acos, asin, sin, cos, pi, sqrt
+from numpy.core.fromnumeric import amin
+from numpy.random import rand
+from numpy import sign
 
 
 from numpy.random import rand
@@ -16,39 +19,74 @@ def rotate(x0,y0,angle):
     return x1,y1
 
 
-def distance(turtle1, turtle2):
+def distance(ax, ay, bx, by):
+    return sqrt((ax-bx)**2 + (ay-by)**2)
+
+
+def turtle_distance(turtle1, turtle2):
     r1 = [turtle1.xcor(),turtle1.ycor()]
     r2 = [turtle2.xcor(),turtle2.ycor()]
     return sqrt((r1[0]-r2[0])**2 + (r1[1]-r2[1])**2)
+
 
 def rebound(ut,un):
     un *= -1
     return ut,un
 
+def update_xy_velocity(impactor,ai,bi):
+    epsilon = bi - ai
+    impactor.dt, impactor.dn = rotate(impactor.dx, impactor.dy, epsilon)
+    impactor.dt, impactor.dn = rebound(impactor.dt, impactor.dn)
+    vmag = sqrt(impactor.dt**2 + impactor.dn**2)
+    br = acos(impactor.dt/vmag)
+    ar = br + bi - ai
+    impactor.dx, impactor.dy = rotate(impactor.dt, impactor.dn, -epsilon)
+    return impactor, ar
 
 
 rp = 10
+rmax = 100
 
-rA = 100*rand()
-rB = 100*rand()
-ru = rA + rp
-xu = ru*(0.5-rand())*2
+if 1: #random case
+    rA = rmax*rand()
+    rB = rmax*rand()
+    rC = rmax*rand()
+    rD = rmax*rand()
+    ru = rA + rp
+    xu = rA*(0.5-rand())*2
+else: #predefined case
+    rA = 100
+    rB = 1
+    rC = 1
+    rD = 1
+    ru = rA + rp
+    xu = -50
+
 
 
 xA = 0
 xB = -(rA+rB)
+xC = rA+rC
+xD = -(rA+2*rB+rD)
+
 ai = pi/4
 vi_mag = 5
 ru = rA + rp
+
+x_imp = 0.0
+y_imp = 0.0
 
 if abs(xu) < ru: #p can collide with particle A
     xuB = (rB+rA)*sin(ai) + xu #shortest distance between centers of p and B
     ruB = rp+rB #shortest distance for p to be able to collide with B
     bi = acos(xu/ru) #set the impaction angle to A particle first
+    will_be_hitted_id = 1
     if abs(xuB) <= ruB: # p hits the B first
         dl_AB = sqrt(ru**2-xu**2) - sqrt(ruB**2-xuB**2) - cos(ai)*(rA+rB)
         if(dl_AB<0): #p hits the B first
             bi = acos(xuB/ruB) #rebound angle, if p hits the A first
+            will_be_hitted_id = 0
+
 
 else: #p does not hit A nor B 
     print("vedle")
@@ -85,53 +123,103 @@ particle_B.penup()
 particle_B.goto(-(rA+rB),0)
 
 
-reboundA = False
-reboundB = False
+particle_C = turtle.Turtle()
+particle_C.shape("circle")
+particle_C.shapesize(rC/10)
+particle_C.color("black")
+particle_C.speed(0)
+particle_C.penup()
+particle_C.goto(xC,0)
+
+particle_D = turtle.Turtle()
+particle_D.shape("circle")
+particle_D.shapesize(rD/10)
+particle_D.color("black")
+particle_D.speed(0)
+particle_D.penup()
+particle_D.goto(xD,0)
+
+
 time = 0
-while time < 100:
+hitted_id = -2
+collision = False
+ar = 0
+ri = [rD, rB, rA, rC]
+xi = [xD, xB, xA, xC]
+
+while time < 80:
     time += 1
     #bounce
-    if distance(impactor,particle_A) <= (rA+rp) and not reboundA:
-        epsilon = bi - ai
-        impactor.dt, impactor.dn = rotate(impactor.dx, impactor.dy, epsilon)
-        impactor.dt, impactor.dn = rebound(impactor.dt, impactor.dn)
-        br = bi
-        ar = br + bi - ai
-        impactor.dx, impactor.dy = rotate(impactor.dt, impactor.dn, -epsilon)
-        print(impactor.dx, impactor.dy)
+    if turtle_distance(impactor,particle_A) <= (rA+rp) and hitted_id != 2:
+        collision = True
+        impactor, ar = update_xy_velocity(impactor,ai,bi)
+        hitted_id = 2
 
-        ai = ar
+    elif turtle_distance(impactor,particle_B) <= (rB+rp) and hitted_id != 1:
+        collision = True
+        impactor, ar = update_xy_velocity(impactor,ai,bi)
+        hitted_id = 1
+    
+    elif turtle_distance(impactor,particle_C) <= (rC+rp) and hitted_id != 3:
+        collision = True
+        impactor, ar = update_xy_velocity(impactor,ai,bi)
+        hitted_id = 3
+    
+    elif turtle_distance(impactor,particle_D) <= (rD+rp) and hitted_id != 0:
+        collision = True
+        impactor, ar = update_xy_velocity(impactor,ai,bi)
+        hitted_id = 0
 
-        reboundA = True
-        reboundB = False
 
-        x_imp = (rA+rp)*sin(epsilon)
-        y_imp = (rA+rp)*cos(epsilon)
-        xu = y_imp*cos(ai) + (x_imp-xA)*sin(ai)
-        if(abs(xu) < (rB+rp)):
-            bi = acos(xu/(rB+rp))
+    #update bi
+    if collision:
+        #calculate coordinates of the center of "p" in the moment of collision
+        x_imp = -(ri[hitted_id]+rp)*sin(bi-ai) + xi[hitted_id]
+        y_imp = (ri[hitted_id]+rp)*cos(bi-ai)
+        #update the particle impaction angle
+        ai = -ar
+        #adjust the particle index according to the direction of particle movement
+        will_be_hitted_id = hitted_id + int(sign(impactor.dx))
+        j = will_be_hitted_id
+        
+        #increment particle indices in the direction of the "p" movement until a collision occurs
+        while -1<j<4:
+            #calculate closest approach of "p" to the j-th particle
+            xu = y_imp*cos(ai) + (x_imp-xi[j])*sin(ai)
+            ru = ri[j]+rp
+            if(abs(xu) < ru): #"p" hits the jth particle
+                bi = acos(xu/ru)
+                break #collision with jth particle will occur, break the loop
 
+            if j==3 and impactor.dy<0 and impactor.dx>0: #particle missed the last particle, but still descends to the surface
+                #shift the last particle's position and alter its diameter
+                xC0 = xC
+                rC0 = rC
+                rC = rmax*rand() #choose new particle's radius
+                xC = xC0+(rC+rC0) #determine shift in the x direction
+                particle_C.goto(xC,0) #shift the patricle
+                particle_C.shapesize(rC/10) #update its radius     
+                xi[3] = xC
+                ri[3] = rC  
 
-    elif distance(impactor,particle_B) <= (rB+rp) and not reboundB:
-        epsilon = bi - ai
-        impactor.dt, impactor.dn = rotate(impactor.dx, impactor.dy, epsilon)
-        impactor.dt, impactor.dn = rebound(impactor.dt, impactor.dn)
-        br = bi
-
-        ar = br + bi - ai
-
-        impactor.dx, impactor.dy = rotate(impactor.dt, impactor.dn, -epsilon)
-
-        ai = ar
-
-        reboundB = True
-        reboundA = False
-
-        x_imp = xB + (rB+rp)*sin(epsilon)
-        y_imp = (rB+rp)*cos(epsilon)
-        xu = y_imp*cos(ai) + (x_imp-xA)*sin(ai)
-        if(abs(xu) < (rA+rp)):
-            bi = acos(abs(xu)/(rA+rp))
+            elif j==0 and impactor.dy<0 and impactor.dx<0: #particle missed the first particle, but still descends to the surface
+                xD0 = xD
+                rD0 = rD
+                rD = rmax*rand() #choose new particle's radius
+                xD = xD0-(rD+rD0) #determine shift in the x direction
+                particle_D.goto(xD,0) #shift the patricle
+                particle_D.shapesize(rD/10) #update its radius     
+                xi[0] = xD
+                ri[0] = rD      
+            
+            else:  #"p" missed the jth particle, go to the next
+                j += int(sign(impactor.dx))
+                
+                
+            will_be_hitted_id = j
+                
+        #"p" leaves the particle's surface, collision ends  
+        collision = False
 
 
     #update impactor position
